3750ce3b8f715a71ca856d50fb699b17212e10a7
PriyankaPal7 <priyankapal243@gmail.com>
2020-07-29 20:04:33 +0530

    first commit 3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("This is test code"); 
+		System.out.println("This is test code.. updated one "); 
 	}
 
 }
